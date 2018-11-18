7a76e3a3a343ef1744278766699fa767e64de250
Conor Edwards <lancerspride@outlook.com>
2018-11-18 01:29:04 -0600

    Added all objects and repos

---
+++ CIS560/Address/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS560.Address
+{
+    public class Address
+    {
+        private int addressId { get; }
+        private string line1 { get; }
+        private string line2 { get; }
+        private string city { get; }
+        private string state { get; }
+        private int zip { get; }
+
+        public Address(int addressId, string line1, string line2, string city, string state, int zip)
+        {
+            this.addressId = addressId;
+            this.line1 = line1;
+            this.line2 = line2;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+        }
+    }
+}
---
+++ CIS560/OrderMenuItem/OrderMenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS560.OrderMenuItem
+{
+    public class OrderMenuItem
+    {
+        private int orderId { get; }
+        private int menuItemId { get; }
+        private int menuItemQuantity { get; }
+
+        public OrderMenuItem(int orderId, int menuItemId, int menuItemQuantity)
+        {
+            this.orderId = orderId;
+            this.menuItemId = menuItemId;
+            this.menuItemQuantity = menuItemQuantity;
+        }
+    }
+}
